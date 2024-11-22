28b63bfe6d475d811df66663d4336ea0b3a873e8
Maxim Shapovalov <syndex@list.ru>
2024-11-22 21:41:26 +0300

    Initial commit

---
+++ src/test/java/syndexmx/github/com/tgsiren/TgsirenApplicationTests.java
@@ -0,0 +1,13 @@
+package syndexmx.github.com.tgsiren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgsirenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
